575cecd54d53ef3466317015a31e7b21a77066c0
Ian Mark Sangcap <ianmarksangcap1318@gmail.com>
2025-05-31 17:30:53 +0800

    change details every sections and add mailer

--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,45 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'ianportfoliomailer@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+    include($php_email_form);
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+
+  require 'PHPMailer/src/PHPMailer.php';
+  require 'PHPMailer/src/SMTP.php';
+  require 'PHPMailer/src/Exception.php';
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
+  
+  $mail = new PHPMailer(true);
+  
+  try {
+      // SMTP Configuration
+      $mail->isSMTP();
+      $mail->Host = 'smtp.gmail.com';
+      $mail->SMTPAuth = true;
+      $mail->Username = 'ianportfoliomailer@gmail.com'; 
+      $mail->Password = 'Qwerty@11#'; 
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+      $mail->Port = 587;
+  
+      // Email Content
+      $mail->setFrom('ianportfoliomailer@gmail.com',  'Ian Portfolio Mailer');
+      $mail->addAddress($_POST['email']);
+      $mail->Subject = $_POST['subject'];
+      $mail->Body = $_POST['message'];
+  
+      // Send Email
+      $mail->send();
+      echo "Email sent successfully!";
+  } catch (Exception $e) {
+      echo "Email could not be sent. Error: {$mail->ErrorInfo}";
+  }
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
